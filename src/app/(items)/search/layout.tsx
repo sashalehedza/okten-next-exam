c433fdd168a3698ca8ff1512e5c06d4159525b09
sashalehedza <sashalehedza@gmail.com>
2024-08-16 20:11:12 +0300

    fix

--- src/app/(items)/search/layout.tsx
+++ src/app/(items)/search/layout.tsx
@@ -7,7 +7,7 @@ const SearchLayout = ({ children }: Props) => {
     <div className={'w-full mx-auto content-center flex flex-col items-center'}>
       <h1 className={'text-6xl text-sky-600'}>Search Movies</h1>
       <div className={'flex justify-center m-8'}>
-        <SearchBar placeholder={'Search Movies ...'} />
+        <SearchBar />
       </div>
       <main>{children}</main>
     </div>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,10 +3,11 @@ import React from 'react'
 import { FaSearch } from 'react-icons/fa'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
-const SearchBar = ({ placeholder }: { placeholder: string }) => {
+const SearchBar = () => {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
+
   const handleSearch = (word: string) => {
     const params = new URLSearchParams(searchParams)
     params.set('page', '1')
@@ -14,37 +15,22 @@ const SearchBar = ({ placeholder }: { placeholder: string }) => {
       params.set('query', word)
     } else {
       params.delete('query')
+      params.delete('page')
     }
     replace(`${pathname}?${params.toString()}`)
   }
+
   return (
-    <form className={'w-[600px] relative'}>
-      <label htmlFor='search' className={'sr-only'}>
-        {' '}
-        Search{' '}
-      </label>
-      <div className={'relative'}>
-        <input
-          className={
-            'w-full text-lg p-4 rounded-full bg-slate-800 placeholder:italic placeholder:text-slate-400 block border border-slate-300  shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
-          }
-          placeholder={placeholder}
-          type='text'
-          name='search'
-          onChange={(e) => {
-            handleSearch(e.target.value)
-          }}
-          defaultValue={searchParams.get('query')?.toString()}
-        />
-        <button
-          className={
-            'invisible sm:visible  absolute right-1 top-1/2 -translate-y-1/2 p-4 rounded-full bg-slate-900'
-          }
-        >
-          <FaSearch />
-        </button>
-      </div>
-    </form>
+    <div className='flex items-center bg-white border border-gray-300 rounded-full px-4 py-2 shadow-md'>
+      <FaSearch className='text-gray-500 mr-3' />
+      <input
+        className='flex-grow outline-none text-gray-700'
+        placeholder={'Search Movies ...'}
+        type='text'
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query') ?? ''}
+      />
+    </div>
   )
 }
 
