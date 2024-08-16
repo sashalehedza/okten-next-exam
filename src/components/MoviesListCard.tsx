59173eecd1513f00e1dd3ef77c30a1b0f4109b6b
sashalehedza <sashalehedza@gmail.com>
2024-08-16 15:59:54 +0300

    fix

--- src/components/MoviesListCard.tsx
+++ src/components/MoviesListCard.tsx
@@ -2,16 +2,44 @@ import React, { FC } from 'react'
 import Image from 'next/image'
 import { Movie } from '@/models/Movie'
 import StarRating from './StarRating'
+import Link from 'next/link'
 
 type IProps = {
   movie: Movie
 }
 
 const MoviesListCard: FC<IProps> = ({ movie }) => {
+  // Map genre IDs to genre names
+
   return (
-    <div>
-      {movie.title}
-      <StarRating rating={movie.vote_average} />
+    <div className='bg-white rounded-lg shadow-lg'>
+      {/* Movie Poster */}
+
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+        className='rounded-md'
+        width={200}
+        height={500}
+        priority
+      />
+
+      <div className='p-4 space-y-4'>
+        <h2 className='text-xl font-semibold mb-2'>{movie.title}</h2>
+        <div className='flex items-center space-x-2 text-gray-500'>
+          <span className='text-sm'>{movie.release_date.split('-')[0]}</span>
+          <span className='text-sm'>•</span>
+        </div>
+        <StarRating rating={movie.vote_average} />
+        <p className='text-gray-700 mt-2'>{movie.overview}</p>
+        <div className='flex items-center justify-between text-gray-600 mt-4'>
+          <span className='text-sm'>
+            Popularity: {movie.popularity.toFixed(1)}
+          </span>
+          <span className='text-sm'>Votes: {movie.vote_count}</span>
+        </div>
+        <Link href={`/movies/${movie.id}`}>More Details</Link>
+      </div>
     </div>
   )
 }
