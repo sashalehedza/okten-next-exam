727ca903af1aebb5e55a0f440f4fbf5189adb1ba
sashalehedza <sashalehedza@gmail.com>
2024-08-16 11:36:20 +0300

    fix

--- src/app/(items)/movies/page.tsx
+++ src/app/(items)/movies/page.tsx
@@ -9,13 +9,7 @@ const UsersPage = async () => {
       <h1 className='text-2xl font-bold mb-6'>Popular Movies</h1>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
         {results.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            title={movie.title}
-            overview={movie.overview}
-            posterPath={movie.poster_path}
-            releaseDate={movie.release_date}
-          />
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,32 +1,25 @@
 import React from 'react'
 import Image from 'next/image'
+import { Movie } from '@/models/Movie'
 
-interface MovieProps {
-  title: string
-  overview: string
-  posterPath: string
-  releaseDate: string
+type MovieCardProps = {
+  movie: Movie
 }
 
-const MovieCard: React.FC<MovieProps> = ({
-  title,
-  overview,
-  posterPath,
-  releaseDate,
-}) => {
+const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
   return (
     <div className='bg-white shadow-md rounded-lg p-4'>
       <Image
-        src={`https://image.tmdb.org/t/p/w500${posterPath}`}
-        alt={title}
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
         className='rounded-md'
         width={500}
         height={500}
         priority
       />
-      <h2 className='text-lg font-semibold mt-2'>{title}</h2>
-      <p className='text-sm text-gray-500'>{releaseDate}</p>
-      <p className='mt-2 text-gray-700'>{overview}</p>
+      <h2 className='text-lg font-semibold mt-2'>{movie.title}</h2>
+      <p className='text-sm text-gray-500'>{movie.release_date}</p>
+      <p className='mt-2 text-gray-700'>{movie.overview}</p>
     </div>
   )
 }
