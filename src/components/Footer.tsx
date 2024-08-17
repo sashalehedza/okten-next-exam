ce6b722bebba691ed4f62548e79403443dfd1a1b
sashalehedza <sashalehedza@gmail.com>
2024-08-17 14:03:04 +0300

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,22 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-gray-800 text-white py-4 mt-2'>
+    <footer className='py-4 mt-2'>
       <div className='container mx-auto text-center'>
         <p>
           &copy; {new Date().getFullYear()} Sasha Lehedza. All rights reserved.
         </p>
         <p>
-          <a href='/privacy-policy' className='text-gray-400 hover:text-white'>
-            Privacy Policy
-          </a>
+          <a href='/privacy-policy'>Privacy Policy</a>
           {' | '}
-          <a
-            href='/terms-of-service'
-            className='text-gray-400 hover:text-white'
-          >
-            Terms of Service
-          </a>
+          <a href='/terms-of-service'>Terms of Service</a>
         </p>
       </div>
     </footer>
