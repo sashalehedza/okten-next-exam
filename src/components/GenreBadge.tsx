5421a2de800ed44ccecd1d9066629f944e5ff39d
sashalehedza <sashalehedza@gmail.com>
2024-08-17 13:41:35 +0300

    fix

--- src/components/GenreBadge.tsx
+++ src/components/GenreBadge.tsx
@@ -1,14 +1,25 @@
+'use client'
+
 import React from 'react'
 import { Genre } from '@/models/Genre'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 type GenreCardProps = {
   genre: Genre
 }
 
 const GenreBadge: React.FC<GenreCardProps> = ({ genre }) => {
+  const pathName = usePathname()
   return (
-    <Link className='shadow-md rounded-lg p-4' href={`/genres/${genre.id}`}>
+    <Link
+      className={`shadow-md rounded-lg p-4 ${
+        pathName === `/genres/${genre.id}`
+          ? 'bg-sky-600 text-light-text'
+          : 'bg-light-background text-light-text dark:bg-dark-background dark:text-dark-text'
+      }`}
+      href={`/genres/${genre.id}`}
+    >
       <h2 className='text-lg font-semibold mt-2'>{genre.name}</h2>
     </Link>
   )
