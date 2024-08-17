88cefbed91f45ed7e268a1eebba6b42670fabab5
sashalehedza <sashalehedza@gmail.com>
2024-08-17 18:22:16 +0300

    fix

--- src/app/(items)/search/layout.tsx
+++ src/app/(items)/search/layout.tsx
@@ -1,12 +1,14 @@
+import React, { Suspense } from 'react'
 import SearchBar from '@/components/SearchBar'
-import React from 'react'
 
 type Props = { children: React.ReactNode }
 const SearchLayout = ({ children }: Props) => {
   return (
     <div className={'w-full mx-auto content-center flex flex-col items-center'}>
       <div className={'flex justify-center m-8'}>
-        <SearchBar />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchBar />
+        </Suspense>
       </div>
       <main>{children}</main>
     </div>
